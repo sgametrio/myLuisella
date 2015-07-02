20e94d9199819282913266bcfc1da288ff4612c6
themetrio <carrarademetrio@gmail.com>
2015-07-02 16:41:04 +0200

    Initial commit for 'kitchen' app

---
+++ kitchen/js/admin.js
@@ -0,0 +1,190 @@
+$(document).ready(function(){
+	//If true that mean we are on mobile and not on localhost.
+	var DEBUG_MOBILE = false;
+	var SERVER_IP = "http://192.168.0.200";
+
+	if(!DEBUG_MOBILE)
+		SERVER_IP = "http://localhost";
+		
+	//Handle click event on submit so the data are sent via AJAX to the PHP's page which save them into DB.
+	$('#submit-button-ingredient').click( function(){
+		$('#ajax-panel-ingredient').empty();
+		var ingredient_name = document.getElementById('ingredient-name').value;
+		var allergen = document.getElementById('allergen').value;
+		if(ingredient_name == "")
+		{
+			$('#ajax-panel-ingredient').append('<strong>Oops!</strong> Check what you wrote.');
+			$('#ajax-panel-ingredient').show();
+			return false;
+		}
+		$.ajax({
+			type: 'POST',
+			url: SERVER_IP + "/myLuisella-server/insertIngredient.php",
+			data: { allergen: allergen, ingredientName: ingredient_name },
+			success:function(data){
+				if(data == "Good")
+				{
+					$('#ajax-panel-ingredient').empty();
+					$('#ajax-panel-ingredient').html('New ingredient inserted. <strong>Congrats!</strong>');
+					$('#ajax-panel-ingredient').show();
+				}
+				else
+				{
+					$('#ajax-panel-ingredient').empty();
+					$('#ajax-panel-ingredient').html(data.toString());
+					$('#ajax-panel-ingredient').show();
+				}
+			},
+			error:function(){
+				// failed request; give feedback to user
+				$('#ajax-panel-ingredient').empty();
+				$('#ajax-panel-ingredient').html('<strong>Oops!</strong> Connection error.');
+				$('#ajax-panel-ingredient').show();
+			}
+		});
+		
+		return false;
+	});	
+    
+	//AJAX -> retrieve categories list
+    $.ajax({
+		type: 'POST',
+		url: SERVER_IP + "/myLuisella-server/retrieveCategoriesList.php",
+		success: function(data){
+			if(data != 0)
+			{
+				var categories = $.parseJSON(data);
+				var str = "";
+				$.each(categories, function(){
+					str = str.concat("<option value='" + this["category"] + "'>" + this["category"] + "</option>");
+				});
+				$('#ajax-panel-plate').empty();
+				$('#category').empty();
+				$('#category').html(str);
+				$('#ajax-panel-plate').hide();
+			}
+			else
+			{
+				$('#ajax-panel-plate').empty();
+				$('#ajax-panel-plate').html(data.toString());
+				$('#ajax-panel-plate').show();
+			}
+		},
+		error:function(){
+			//failed request; give feedback to user
+			$('#ajax-panel-plate').empty();
+			$('#ajax-panel-plate').html('<strong>Oops!</strong> Connection error.');
+			$('#ajax-panel-plate').show();
+		}
+		
+	});
+	
+	//AJAX -> retrieve plates list
+    $.ajax({
+		type: 'POST',
+		url: SERVER_IP + "/myLuisella-server/retrievePlatesList.php",
+		success: function(data){
+			if(data != 0)
+			{
+				var plates = $.parseJSON(data);
+				var str = "";
+				$.each(plates, function(){
+					str = str.concat("<option value='" + this["foodId"] + "'>" + this["foodName"] + "</option>");
+				});
+				$('#ajax-panel-list').empty();
+				$('#plate-list').empty();
+				$('#plate-list').html(str);
+				$('#ajax-panel-list').hide();
+			}
+			else
+			{
+				$('#ajax-panel-list').empty();
+				$('#ajax-panel-list').html(data.toString());
+				$('#ajax-panel-list').show();
+			}
+		},
+		error:function(){
+			//failed request; give feedback to user
+			$('#ajax-panel-list').empty();
+			$('#ajax-panel-list').html('<strong>Oops!</strong> Connection error.');
+			$('#ajax-panel-list').show();
+		}
+		
+	});
+	
+	//AJAX -> retrieve plates list
+    $.ajax({
+		type: 'POST',
+		url: SERVER_IP + "/myLuisella-server/retrieveIngredientsList.php",
+		success: function(data){
+			if(data != 0)
+			{
+				var plates = $.parseJSON(data);
+				var str = "";
+				$.each(plates, function(){
+					str = str.concat("<option value='" + this["ingredientId"] + "'>" + this["ingredientName"] + "</option>");
+				});
+				$('#ajax-panel-list').empty();
+				$('#ingredient-list').empty();
+				$('#ingredient-list').html(str);
+				$('#ajax-panel-list').hide();
+			}
+			else
+			{
+				$('#ajax-panel-list').empty();
+				$('#ajax-panel-list').html(data.toString());
+				$('#ajax-panel-list').show();
+			}
+		},
+		error:function(){
+			//failed request; give feedback to user
+			$('#ajax-panel-list').empty();
+			$('#ajax-panel-list').html('<strong>Oops!</strong> Connection error.');
+			$('#ajax-panel-list').show();
+		}
+		
+	});
+	
+	
+	//Handle click event on submit so the data are sent via AJAX to the PHP's page which save them into DB.
+	$('#submit-button-plate').click(function(){
+		$('#ajax-panel-plate').hide();
+		var food_name = document.getElementById('food-name').value;
+		var food_price = document.getElementById('food-price').value;
+		var description = document.getElementById('description').value;
+		var category = document.getElementById('category').value;
+		if(food_name == "" || food_price == "")
+		{
+			$('#ajax-panel-plate').html('<strong>Oops!</strong> Check what you wrote.');
+			$('#ajax-panel-plate').show();
+			return false;
+		}
+		$.ajax({
+			type: 'POST',
+			url: SERVER_IP + "/myLuisella-server/insertPlate.php",
+			
+			data: { foodName: food_name, price: food_price, description: description, category: category },
+			success:function(data){
+				if(data == "Good")
+				{
+					$('#ajax-panel-plate').empty();
+					$('#ajax-panel-plate').html('New plate added. <strong>Congrats!</strong>');
+				}
+				else
+				{
+					$('#ajax-panel-plate').empty();
+					$('#ajax-panel-plate').html(data.toString());
+				}
+				$('#ajax-panel-plate').show();
+			},
+			error:function(){
+				// failed request; give feedback to user
+				$('#ajax-panel-plate').empty();
+				$('#ajax-panel-plate').html('<strong>Oops!</strong> Connection error.');
+				$('#ajax-panel-plate').show();
+			}
+			
+		});
+		return false;
+	});
+});
---
+++ kitchen/js/app.js
@@ -0,0 +1,158 @@
+$(document).ready(function(){
+	//If true that mean we are on mobile and not on localhost.
+	var DEBUG_MOBILE = false;
+	var SERVER_IP = "http://192.168.0.200";
+
+	if(!DEBUG_MOBILE)
+		SERVER_IP = "http://localhost";
+		
+	//Refresh button
+	$('#all-tables-and-orders').click(all_tables);
+	
+	function all_tables(){
+	
+		$.ajax({
+			type: 'GET',
+			url: SERVER_IP + "/myLuisella-server/tablesAndOrders.php",
+			success:function(data){
+				if(data == 0)
+				{
+					//It means there aren't any tables
+					$('#tables').html("No tables found.");
+				}
+				else
+				{
+					//We parse JSON to display tables into cards
+					var tables = $.parseJSON(data);
+					for(var i = 1; i <= 3; i++)
+						$('#tables-col-' + i).empty();
+					for(var i = 0; i < tables.length; i++)
+					{
+						var str = "";
+						var n_order = tables[i]["order"].length;
+						if(n_order == 0)
+						{
+							str = str.concat("<div class='panel panel-info'><div style='text-align:center' class='panel-heading'><div style='float:left'>#" + tables[i]["tableNumber"] + "</div>" + tables[i]["tableName"] + "</div><div class='panel-body'><p id='" + tables[i]["tableId"] + "' style='text-align:center'><strong>No orders found yet.</strong></p></div></div>");
+						}
+						else
+						{
+							str = str.concat("<div class='panel panel-info'><div style='text-align:center' class='panel-heading'><div style='float:left'>#" + tables[i]["tableNumber"] + "</div>" + tables[i]["tableName"] + "</div><div class='panel-body'><p><table id='" + tables[i]["tableId"] + "' class='table table-condensed table-striped'>");
+							for(var j = 0; j < n_order; j++)
+							{
+								for(var k = 0; k < tables[i]["order"][j]["foodIds"].length; k++)
+								{
+									str = str.concat("<tr data-toggle='modal' data-food='" + tables[i]["order"][j]["foodNames"][k] + "' data-qty='" + tables[i]["order"][j]["quantities"][k] + "' data-table='" + tables[i]["tableId"] + "' data-id='" + tables[i]["order"][j]["foodIds"][k] + "&" + tables[i]["order"][j]["orderId"] + "' data-target='#mark-as-done'><td class='col-md-9 col-sm-9 col-xs-9'>" + tables[i]["order"][j]["foodNames"][k] + "</td>");
+									if(tables[i]["order"][j]["extraInfos"][k] != null)
+										str = str.concat("<td class='col-md-1 col-sm-1 col-xs-1'>X</td>");
+									else
+										str = str.concat("<td class='col-md-1 col-sm-1 col-xs-1'></td>");
+									str = str.concat("<td class='col-md-2 col-sm-2 col-xs-2'>" + tables[i]["order"][j]["quantities"][k] + "</td>");
+									str = str.concat("</tr>");
+								}
+							}
+							str = str.concat("</table></p></div></div>");
+						}
+						var column = ((i%3)+1);
+						$(str).appendTo('#tables-col-' + column);
+					}
+				}
+				
+			},
+			error:function(){
+				// failed request; give feedback to user
+				$('#tables').html('<strong>Oops!</strong> Try that again in a few moments.');
+			}
+		});
+	}
+	
+	function markPlateAsDone(table_id, foodId, orderId, modal)
+	{
+		$.ajax({
+			type: 'POST',
+			url: SERVER_IP + "/myLuisella-server/markPlateAsDone.php",
+			data: { foodId: foodId, orderId: orderId },
+			success: function(data){
+				if(data == 0)
+				{
+					modal.modal('hide');
+					refreshTableOrder(table_id);
+				}
+				else
+				{
+					modal.find('#modal-body').html('DataBase error. Please retry.');
+				}
+			},
+			error:function(){
+				modal.find('#modal-body').html('No connection to the server. Check it and retry soon.');
+			}
+		});
+	}
+	
+	$('#mark-as-done').modal({
+		show: false
+	}).on('show.bs.modal', function(event){
+		var getIdFromRow = $(event.relatedTarget).data('id');
+		var foodName = $(event.relatedTarget).data('food');
+		var quantity= $(event.relatedTarget).data('qty');
+		var table_id = $(event.relatedTarget).data('table');
+		var modal = $(this);
+        //make your ajax call populate items or what even you need
+        modal.find('#modal-body').html($('<b> Order selected: ' + quantity + ' ' + foodName  + '</b>'))
+		$('#mark-button').click(function(){
+			//splitting id into foodId and orderId
+			var ids = getIdFromRow.split('&');
+			//ids[0] -> foodId | ids[1] -> orderId
+			var foodId = ids[0];
+			var orderId = ids[1];
+			markPlateAsDone(table_id, foodId, orderId, modal);
+		});
+	});
+    
+	//Simulating #all-tables-and-orders click to view 
+	$('#all-tables-and-orders').trigger('click');
+	
+	
+	function refreshTableOrder(table_id)
+	{
+		$.ajax({
+			type: 'POST',
+			url: SERVER_IP + "/myLuisella-server/findOrders.php",
+			data: { tableId: table_id, toMake: "1" },
+			success:function(data) {
+				var str = "";
+				if(data == 0)
+					str = "<div style='text-align:center'><strong>No orders found yet.</strong></div>";
+				else
+				{
+					var orders = $.parseJSON(data);
+				$('#' + table_id).empty();
+				
+				if(orders.length == 0)
+					str = "<div style='text-align:center'><strong>No orders found yet.</strong></div>";
+				else
+					str = str.concat("<tr><th class='col-md-9 col-sm-9 col-xs-9'>Plate name</th><th class='col-md-1 col-sm-1 col-xs-1'></th><th class='col-md-2 col-sm-2 col-xs-2'>Q.ty</th></tr>");
+					for(var j = 0; j < orders.length; j++)
+					{
+						for(var k = 0; k < orders[j]["foodIds"].length; k++)
+						{
+							str = str.concat("<tr data-toggle='modal' data-food='" + orders[j]["foodNames"][k] + "' data-qty='" + orders[j]["quantities"][k] + "' data-table='" + table_id + "' data-id='" + orders[j]["foodIds"][k] + "&" + orders[j]["orderId"] + "' data-target='#mark-as-done'><td class='col-md-9 col-sm-9 col-xs-9'>" + orders[j]["foodNames"][k] + "</td>");
+							if(orders[j]["extraInfos"][k] != null)
+								str = str.concat("<td class='col-md-1 col-sm-1 col-xs-1'>X</td>");
+							else
+								str = str.concat("<td class='col-md-1 col-sm-1 col-xs-1'></td>");
+							str = str.concat("<td class='col-md-2 col-sm-2 col-xs-2'>" + orders[j]["quantities"][k] + "</td>");
+							str = str.concat("</tr>");
+						}
+					}
+				}
+				
+				$('#' + table_id).html(str);
+			},
+			error:function(){
+			}
+		});
+	}
+	
+	setInterval(all_tables, 15000);
+});
+
